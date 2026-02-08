4631e36c32a27fbde770481e7318caa004013be2
MidnightHour1 <DCaria@hotmail.com>
2026-02-08 20:19:47 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const movies = [
+ { title:"The Thing (1982)", poster:"https://via.placeholder.com/160x240?text=The+Thing" },
+ { title:"Halloween (1978)", poster:"https://via.placeholder.com/160x240?text=Halloween" },
+ { title:"Alien (1979)", poster:"https://via.placeholder.com/160x240?text=Alien" },
+ { title:"Blade Runner (1982)", poster:"https://via.placeholder.com/160x240?text=Blade+Runner" },
+ { title:"Nightmare on Elm Street", poster:"https://via.placeholder.com/160x240?text=Nightmare" }
+];
+
+const row = document.getElementById("movieRow");
+
+movies.forEach(movie => {
+  const card = document.createElement("div");
+  card.className = "movie-card";
+  card.innerHTML = `
+    <img src="${movie.poster}">
+    <p>${movie.title}</p>
+  `;
+  row.appendChild(card);
+});
