3288b65eea5c1574aef5858d49331cbbecbf28f8
MidnightHour1 <DCaria@hotmail.com>
2026-02-08 21:00:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const movies = [
-  {
-    title: "The Thing (1982)",
-    poster: "https://image.tmdb.org/t/p/w300/rtF0zC4J5cP2xwqP3h2xY9Yx.jpg",
-    trailer: "https://www.youtube.com/embed/5ftmr17M-a4"
-  },
-  {
-    title: "Halloween (1978)",
-    poster: "https://image.tmdb.org/t/p/w300/vjoOFOTBJcJvA1weJejlZ92LZD4.jpg",
-    trailer: "https://www.youtube.com/embed/xHuOtLTQ_1I"
-  }
+{
+ title:"The Thing (1982)",
+ poster:"https://image.tmdb.org/t/p/w300/rtF0zC4J5cP2xwqP3h2xY9Yx.jpg",
+ trailer:"https://www.youtube.com/embed/5ftmr17M-a4"
+},
+{
+ title:"Halloween (1978)",
+ poster:"https://image.tmdb.org/t/p/w300/vjoOFOTBJcJvA1weJejlZ92LZD4.jpg",
+ trailer:"https://www.youtube.com/embed/xHuOtLTQ_1I"
+}
 ];
 
 const row = document.getElementById("movieRow");
@@ -16,18 +16,15 @@ const mainTrailer = document.getElementById("mainTrailer");
 
 row.innerHTML = "";
 
-movies.forEach(movie => {
+movies.forEach(movie=>{
   const card = document.createElement("div");
   card.className = "movie-card";
-
   card.innerHTML = `
     <img src="${movie.poster}">
     <p>${movie.title}</p>
   `;
-
   card.onclick = () => {
     mainTrailer.src = movie.trailer;
   };
-
   row.appendChild(card);
 });
