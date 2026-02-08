121fafe9b716bb2afe8f7c8260b36eaa139c2ee6
MidnightHour1 <DCaria@hotmail.com>
2026-02-08 20:36:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const movies = [
- { title:"The Thing (1982)", poster:"https://via.placeholder.com/160x240?text=The+Thing" },
- { title:"Halloween (1978)", poster:"https://via.placeholder.com/160x240?text=Halloween" },
- { title:"Alien (1979)", poster:"https://via.placeholder.com/160x240?text=Alien" },
- { title:"Blade Runner (1982)", poster:"https://via.placeholder.com/160x240?text=Blade+Runner" },
- { title:"Nightmare on Elm Street", poster:"https://via.placeholder.com/160x240?text=Nightmare" }
+  { title:"The Thing (1982)", poster:"https://image.tmdb.org/t/p/w300/rtF0zC4J5cP2xwqP3h2xY9Yx.jpg" },
+  { title:"Halloween (1978)", poster:"https://via.placeholder.com/160x240?text=Halloween" },
+  { title:"Alien (1979)", poster:"https://via.placeholder.com/160x240?text=Alien" },
+  { title:"Blade Runner (1982)", poster:"https://via.placeholder.com/160x240?text=Blade+Runner" },
+  { title:"Nightmare on Elm Street", poster:"https://via.placeholder.com/160x240?text=Nightmare" }
 ];
 
 const row = document.getElementById("movieRow");
@@ -17,3 +17,4 @@ movies.forEach(movie => {
   `;
   row.appendChild(card);
 });
+
