c0c7d5af0cc79d535af99b7da62a378f30b651e5
MidnightHour1 <DCaria@hotmail.com>
2026-02-08 20:55:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const row = document.getElementById("movieRow");
+const movies = [
+  {
+    title: "The Thing (1982)",
+    poster: "https://image.tmdb.org/t/p/w300/rtF0zC4J5cP2xwqP3h2xY9Yx.jpg",
+    trailer: "https://www.youtube.com/embed/5ftmr17M-a4"
+  },
+  {
+    title: "Halloween (1978)",
+    poster: "https://image.tmdb.org/t/p/w300/vjoOFOTBJcJvA1weJejlZ92LZD4.jpg",
+    trailer: "https://www.youtube.com/embed/xHuOtLTQ_1I"
+  }
+];
 
-row.innerHTML = `
-  <div class="movie-card">
-    <img src="https://image.tmdb.org/t/p/w300/rtF0zC4J5cP2xwqP3h2xY9Yx.jpg">
-    <p>The Thing (1982)</p>
-  </div>
-`;
+const row = document.getElementById("movieRow");
+const mainTrailer = document.getElementById("mainTrailer");
+
+row.innerHTML = "";
+
+movies.forEach(movie => {
+  const card = document.createElement("div");
+  card.className = "movie-card";
+
+  card.innerHTML = `
+    <img src="${movie.poster}">
+    <p>${movie.title}</p>
+  `;
+
+  card.onclick = () => {
+    mainTrailer.src = movie.trailer;
+  };
+
+  row.appendChild(card);
+});
